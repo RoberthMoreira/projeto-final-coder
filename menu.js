a0f336f6f8a99a5ddcbfe526fe15abe0df83cf86
Roberth Moreira <Roberthmoreiradg9@gmail.com>
2024-08-17 16:56:57 -0300

    manu lateral

---
+++ menu.js
@@ -0,0 +1,7 @@
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('header2')
+
+btnMenu.addEventListener('click',()=>{
+    menu.classList.add('abrir-menu')
+})
