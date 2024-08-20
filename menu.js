b322164da7d223effa2060f55b9b646b8669651c
Roberth Moreira <Roberthmoreiradg9@gmail.com>
2024-08-20 01:11:56 -0300

    menu lateral

--- menu.js
+++
@@ -1,7 +0,0 @@
-
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('header2')
-
-btnMenu.addEventListener('click',()=>{
-    menu.classList.add('abrir-menu')
-})
